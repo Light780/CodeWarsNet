07c9038d345b88be48fa56beacf0019dcbf0fbe8
Bruno Ramos <70899175+Light780@users.noreply.github.com>
2023-02-27 11:08:36 -0500

    Update

---
+++ Capitalize/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string Capitalize(string s, List<int> idxs)
+{
+    return new string(s
+        .Select((c, charIndex) => idxs.Any(idx => idx == charIndex) ? char.ToUpper(c) : c)
+        .ToArray());
+}
+
+
+Console.WriteLine(Capitalize("abcdef", new List<int> {1, 2, 5}));
---
+++ GreedIsGood/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+static int Score(int[] dice)
+{
+    var scores = new Dictionary<int, int>
+    {
+        {1, 1000}, 
+        {6, 600}, 
+        {5, 500}, 
+        {4, 400}, 
+        {3, 300}, 
+        {2, 200}
+    };
+    var dices = new Dictionary<int, int>();
+    var total = 0;
+    foreach (var number in dice)
+    {
+        if(!dices.ContainsKey(number))
+            dices.Add(number, dice.Count(x => x == number));
+    }
+    foreach (var (key, value) in dices)
+    {
+        if (value >= 3)
+            total += scores[key] * (value / 3);
+        if (key == 1)
+            total += 100 * (value % 3);
+        if (key == 5)
+            total += 50 * (value % 3);
+    }
+
+    return total;
+}
+
+Console.WriteLine(Score(new int[]{2, 4, 4, 5, 4}));
